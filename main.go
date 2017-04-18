429e1774e9a323e36dadc35f86af713b4c7192c0
André Carvalho <asantostc@gmail.com>
2017-04-18 18:23:08 -0300

    instrument open connections on server

--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/urfave/negroni"
 )
+
+var (
+	openConnections = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "myapp_open_connections",
+		Help: "The current number of open connections.",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(openConnections)
+}
 
 func main() {
 	s := &http.Server{
@@ -16,6 +29,14 @@ func main() {
 		Handler:      router(),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
+		ConnState: func(c net.Conn, s http.ConnState) {
+			switch s {
+			case http.StateNew:
+				openConnections.Inc()
+			case http.StateHijacked | http.StateClosed:
+				openConnections.Dec()
+			}
+		},
 	}
 	log.Println("HTTP server listening at :8080...")
 	log.Fatal(s.ListenAndServe())
